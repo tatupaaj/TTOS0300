15cd988eace75127173b7c2e85a10e3b9308da05
Tatu <Tatu@P52>
2017-02-06 18:48:06 +0200

    commit

---
+++ wpfHello/Tehtävät 9-1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtävät_9_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int laskuri1;
+        private int laskuri2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            laskuri1 = 0;
+            textBlock2.Text = laskuri1.ToString();
+            laskuri2 = 0;
+            textBlock3.Text = laskuri2.ToString();
+
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = " " + textBlock2.Text;
+            laskuri1++;
+            textBlock2.Text = laskuri1.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock3.Text = " " + textBlock3.Text;
+            laskuri2++;
+            textBlock3.Text = laskuri2.ToString();
+        }
+    }
+}
---
+++ wpfHello/Tehtävät 9-2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtävät_9_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double luku1;
+        private double luku2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            luku2 = Double.Parse(luku2.Text);
+            luku1 = 
+
+            if ((bool)markka.IsCheked) muunnos = luku1 / 5.94573;
+        }
+    }
+}
